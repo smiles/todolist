31fec0e461d9e3f9830bf42f26cff20c532a4a92
smiles <smiles@smilesdow.smi>
2020-02-11 07:24:10 -0500

    created elixir project

---
+++ lib/todolist.ex
@@ -0,0 +1,18 @@
+defmodule Todolist do
+  @moduledoc """
+  Documentation for `Todolist`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Todolist.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
